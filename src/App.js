92d06d9b418a7a7d770550df9e40fde1e1fcf207
fan777 <fan777@gmail.com>
2021-10-05 19:58:30 -0700

    joke class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import JokeList from "./JokeList";
+import JokeListClass from "./JokeListClass";
 
-function App() {
-  return (
-    <div className="App">
-      <JokeList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <JokeListClass />
+      </div>
+    );
+  }
 }
 
 export default App;
